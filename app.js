71cd5272df78f89c3feaf2ce5528f075fdee58ab
Arsh <Codetheon@users.noreply.github.com>
2020-09-27 02:33:04 +0530

    Sign Up page made , Firebase Database linked to the server and database created and CSS modified

--- app.js
+++ app.js
@@ -2,6 +2,41 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var ejs = require('ejs')
 
+var admin = require("firebase-admin");
+
+var serviceAccount = "PsyHealth_Service_key.json"
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://psyhealth-c07b6.firebaseio.com"
+});
+
+var registeredData = {}
+var registeredKeys = []
+var appointmentData = {}
+var appointmentKeys = []
+
+var database = admin.database();
+var signupDatabase =  database.ref("Signup_Data")
+var appointmentDatabase = database.ref("Appointment_Data")
+
+signupDatabase.on('value',(data)=>{
+    registeredData = (data.val())
+    registeredKeys = Object.keys(registeredData)
+},(err)=>{
+   console.log("Error!")
+   console.log(err)
+})
+
+appointmentDatabase.on('value',(data)=>{
+    appointmentData = (data.val());
+    appointmentKeys = Object.keys(appointmentData);
+},(err)=>{
+    console.log("Error!")
+    console.log(err)
+})
+
+
 var app = express()
 var port = 3000 || process.env.port
 
@@ -23,6 +58,20 @@ app.get('/login',(req,res)=>{
 app.get('/signup',(req,res)=>{
     res.render('signup')
 })
+
+app.post("/signup",(req,res)=>{
+    var signupData = {
+        name: req.body.name,
+        email:req.body.email,
+        pass:req.body.password,
+        gender:req.body.gender,
+        dob:req.body.dob,
+        slot : req.body.slot
+    }
+    console.log(signupData)
+    signupDatabase.push(signupData)
+})
+
 app.get('/services',(req,res)=>{
     res.render('services')
 })
